3fbccb0739bc48161b4c2a582cba13a3d79c4897
ceda-dev-01 <l.lezzerini@coopceda.it>
2021-07-16 09:41:02 +0200

    Client server vuoti

---
+++ server/src/main/java/it/corso/listaspesa/ListaspesaApplication.java
@@ -0,0 +1,13 @@
+package it.corso.listaspesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaspesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaspesaApplication.class, args);
+	}
+
+}
